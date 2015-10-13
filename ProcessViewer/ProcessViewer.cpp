97c1915730b133c39225a9bb19aa48b2c796bf93
HyoWon Jang <jang.hw73@gmail.com>
2015-10-13 12:05:35 +0900

    make test

--- ProcessViewer/ProcessViewer.cpp
+++ ProcessViewer/ProcessViewer.cpp
@@ -20,10 +20,14 @@ int main()
 	char pname[1024] = { 0 };
 	size_t convertedChars = 0;
 	int process_cnt = 0;
+	LPTSTR szNotepad = _tcsdup(TEXT("winword.exe"));
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
 	while (b)
 	{
-		printf_s("process Name:%S\n", ppe.szExeFile);
-		printf_s("process ID:%6d\n", ppe.th32ProcessID);
+		//printf_s("process Name:%S\n", ppe.szExeFile);
+		//printf_s("process ID:%6d\n", ppe.th32ProcessID);
 		b = Process32Next(hSnap, &ppe);
 		wcstombs_s(&convertedChars, pname, sizeof(ppe.szExeFile), ppe.szExeFile, _TRUNCATE);
 		if (strcmp(pname, "PING.EXE") == 0)
@@ -39,15 +43,20 @@ int main()
 	if (!proc_tree.build_process_tree()) return false;
 
 	// 프로세스 열거 테스트 (by callback)
-	//proc_tree.iterate_process(proc_tree_callback, 0);
+	proc_tree.iterate_process(proc_tree_callback, 0);
 	//proc_tree.iterate_process_tree(proc_tree.find_process(L"System"), proc_tree_callback, 0);
+	getchar();
 
 	// print 
-	proc_tree.print_process_tree((DWORD)596);
+	//proc_tree.print_process_tree((DWORD)596);
+	ShellExecute(NULL, _tcsdup(TEXT("open")), szNotepad, NULL, NULL, SW_SHOW);
 
+	getchar();
+	proc_tree.build_process_tree();
 	// 프로세스 종료 테스트	
-	//proc_tree.kill_process_tree(proc_tree.find_process(L"cmd.exe"));
+	proc_tree.kill_process_tree(proc_tree.find_process(L"winword.exe"));
 
+	
 	//프로세스 일시정지 테스트
 	if (process_cnt > 0)
 	{
@@ -58,7 +67,8 @@ int main()
 			return -1;
 		}
 		printf("success!! wait...\n");
-		Sleep(5000);
+		getchar();
+		//Sleep(5000);
 		//프로세스 일시정지후 다시 실행
 		printf("resuming...\n");
 		if (!proc.resume(suspend_pid))
@@ -72,7 +82,10 @@ int main()
 	{
 		printf("process가 존재하지 않습니다!\n");
 	}
-	return true;
-	
+
+	getchar();
+
+	return 0;
 }
 
+ 
